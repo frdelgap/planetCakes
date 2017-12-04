6b8dbca453a12d0023b359d44965f523505ec2ae
frdelgap <31180200+frdelgap@users.noreply.github.com>
2017-12-04 01:25:23 +0100

    change app.js

--- app.js
+++ app.js
@@ -26,12 +26,13 @@ mongoose.connect(process.env.MONGODB_URI);
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
